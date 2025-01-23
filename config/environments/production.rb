a006252e2cf8cac9a6689aebd4e45d54c9b5d862
谷本直輝 <tanimotonaoki@MacBook-Air.local>
2025-01-23 19:42:04 +0900

    メール認証2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "test-tf.onrender.com", port: 3000 }
+  config.action_mailer.default_url_options = { host: "test-tf.onrender.com", protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:"smtp.gmail.com",
