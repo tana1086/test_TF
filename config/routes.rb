24d2751bd33cf7505ae6c47a52153dfe6bcb9ae6
谷本直輝 <tanimotonaoki@MacBook-Air.local>
2025-01-25 08:01:10 +0900

    fix:認証メール修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
